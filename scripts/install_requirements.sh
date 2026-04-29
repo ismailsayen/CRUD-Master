c741e6315c52cd7b42eb17d44c8c5babf719db1a
ismailsayen <ismailsvn02@gmail.com>
2026-04-29 17:35:28 +0100

    Add script to automate service environment setup
    Provides a streamliedn way to initialize Python virtual environments and install dependencies across all application modules. This ensures consistent development environments and simplifies the initial setup process for the project services.

---
+++ scripts/install_requirements.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+home="/home/isayen/Desktop/CRUD-Master/srcs"
+
+services=( "/api-gateway-app" "/billing-app" "/inventory-app" )
+
+for d in "${services[@]}"; do
+ 
+cd $home$d
+
+python3 -m venv envs
+
+source ./envs/activate
+
+pip install -r requirements.txt
+
+deactivate
+
+done
+
+
+
